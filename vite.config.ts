41c49f5d349b3c4b30063ce6485f0f57f9b96d1e
turtle <imstevenleo1999@gmail.com>
2025-07-21 20:51:29 +0300

    Improve responsiveness and alignment

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import autoprefixer from 'autoprefixer' // إضافة Autoprefixer
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './', // ✅ تضبط كل المسارات لتكون relative
+  base: '/', // ✅ الحل لمشكلة Vercel
   plugins: [
     vue(),
     vueDevTools(),
@@ -13,6 +14,13 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer(), // إضافة Autoprefixer كمكوّن PostCSS
+      ],
     },
   },
   build: {
