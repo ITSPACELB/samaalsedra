513bd4dc28f1eb0bb2eceae636eac4e0d5a5b45d
turtle <imstevenleo1999@gmail.com>
2025-07-22 11:56:49 +0300

    change3

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,8 @@
 // vue.config.js
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
   filenameHashing: true,
   css: {
-    extract: true // يستخرج CSS إلى ملفات منفصلة لتقليل المشاكل في العرض
+    extract: true
   }
-}
+};
