f187df89eab2a9605cd8144be9ea3ebeb99063ca
turtle <imstevenleo1999@gmail.com>
2025-07-22 05:55:30 +0300

    change in main and vit

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import autoprefixer from 'autoprefixer' // إضافة Autoprefixer
+import autoprefixer from 'autoprefixer'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/', // ✅ الحل لمشكلة Vercel
+  base: '/', // حل مشكلة المسارات على Vercel
   plugins: [
     vue(),
     vueDevTools(),
@@ -19,9 +19,10 @@ export default defineConfig({
   css: {
     postcss: {
       plugins: [
-        autoprefixer(), // إضافة Autoprefixer كمكوّن PostCSS
+        autoprefixer(),
       ],
     },
+    // في Vite ما في css.loaderOptions.css.url، إذا عندك مشكلة بالمسارات لازم تحلها بطريقة ثانية
   },
   build: {
     rollupOptions: {
