ef9e5ad1bad1012a692be5b91213e9915392bd2f
turtle <imstevenleo1999@gmail.com>
2025-08-28 05:08:04 +0300

    first with claude2

--- env.d.ts
+++
@@ -1,9 +0,0 @@
-interface ImportMetaEnv {
-  readonly VITE_OPENAI_API_KEY: string;
-  readonly VITE_WEATHER_API_KEY: string;
-  readonly BASE_URL: string; // ✅ أضفنا هيدا السطر
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
